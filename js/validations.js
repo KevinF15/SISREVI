8e51ca8c86a84f04f3526f047e4fe48e077ac822
MRP5 <64103655+MRP5@users.noreply.github.com>
2021-09-15 08:03:06 -0400

    Add files via upload

--- js/validations.js
+++ js/validations.js
@@ -15,7 +15,7 @@ $(function () {
 			required: true,
 			re: /^[A-Za-z0-9\u00f1\u00d1\u00E0-\u00FC\s]{0,100}$/,
 			message: "Se ha excedido el limite de caracteres permitidos (maximo: 100).",
-		}
+		},
 	}
 
 	// Verificar lo escrito en cada <input> / <textarea>
@@ -39,4 +39,93 @@ $(function () {
 	    	}
 		}
 	});
+});
+
+$(document).ready(function(){
+
+	$("#pnameInput").on("keypress",function(e){
+		codigo = e.keyCode;
+		er = /^[A-Za-z0-9-#_ \b]*$/;
+		tecla = String.fromCharCode(codigo);
+		bien = er.test(tecla);
+		if(!bien){
+			e.preventDefault();
+		}
+	});
+	
+	$("#pnameInput").on("keyup",function(){
+		er = /^[A-Za-z0-9-#_ \b]{3,40}$/;
+		bien = er.test($(this).val());
+		if(bien){
+			$("#spnameInput").text("");
+		}
+		else{
+			$("#spnameInput").text("Min 3 caracteres - Max 40 caracteres");
+		}
+
+	});
+	
+	$("#telfInpu").on("keypress",function(e){
+		codigo = e.keyCode;
+		er = /^[0-9\b]*$/;
+		tecla = String.fromCharCode(codigo);
+		bien = er.test(tecla);
+		if(!bien){
+			e.preventDefault();
+		}
+	});
+	
+	$("#telfInpu").on("keyup",function(){
+		er = /^[0-9\b]{11}$/;
+		bien = er.test($(this).val());
+		if(bien){
+			$("#stelfInpu").text("");
+		}
+		else{
+			$("#stelfInpu").text("Solo 11 digitos");
+		}
+	});
+
+	$("#pdescInput").on("keypress",function(e){
+		codigo = e.keyCode;
+		er = /^[A-Za-z0-9-#_ \b]*$/;
+		tecla = String.fromCharCode(codigo);
+		bien = er.test(tecla);
+		if(!bien){
+			e.preventDefault();
+		}
+	});
+	
+	$("#pdescInput").on("keyup",function(){
+		er = /^[A-Za-z0-9-#_ \b]{0,100}$/;
+		bien = er.test($(this).val());
+		if(bien){
+			$("#spdescInput").text("");
+		}
+		else{
+			$("#spdescInput").text("Max 100 caracteres");
+		}
+	});
+
+	$("#codeInput").on("keypress",function(e){
+		codigo = e.keyCode;
+		er = /^[Jj0-9-\b]*$/;
+		tecla = String.fromCharCode(codigo);
+		bien = er.test(tecla);
+		if(!bien){
+			e.preventDefault();
+		}
+	});
+	
+	$("#codeInput").on("keyup",function(){
+		er = /^[Jj0-9-\b]{9,10}$/;
+		bien = er.test($(this).val());
+		if(bien){
+			$("#scodeInput").text("");
+		}
+		else{
+			$("#scodeInput").text("J-0000000");
+		}
+	});
+
 });
