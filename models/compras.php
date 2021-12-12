3a94f727be09d9c99c559f544127c135140007c8
MRP5 <64103655+MRP5@users.noreply.github.com>
2021-12-12 03:00:52 -0400

    Add files via upload

--- models/compras.php
+++ models/compras.php
@@ -22,5 +22,41 @@
 				return $e->getMessage();
 			}
 	 	}
+
+		public function showProducs() {
+			$BD = $this->connectBD();
+			$BD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			try {
+				$producs = $BD->query("Select * from productos");
+
+				if ($producs) {
+					$results = '';
+
+					foreach ($producs as $produc) {
+						$results = $results."<tr style='cursor:pointer' onclick='colocaproducto(this);'>";
+							$results = $results."<td>";
+								$results = $results.$produc['nombre']; 
+							$results = $results."</td>";
+							$results = $results."<td>";
+								$results = $results.$produc['descripcion'];
+							$results = $results."</td>";
+							$results = $results."<td>";
+								$results = $results.$produc['existencia'];
+							$results = $results."</td>";
+							$results = $results."<td>";
+								$results = $results.$produc['prec_proveedor'];
+							$results = $results."</td>";
+						$results = $results."</tr>";
+					}
+
+					return $results;
+				}
+			} catch(Exception $e){
+			   return $e->getMessage();
+		   }
+		}
 	 }
+
+	 
 ?>
