263bc5a9372f10d9c7865607ae72bb4b7e08c036
MRP5 <64103655+MRP5@users.noreply.github.com>
2022-02-19 20:32:25 -0400

    Add files via upload

--- controllers/historial_compras.php
+++ controllers/historial_compras.php
@@ -1,6 +1,14 @@
 <?php
+	if (!is_file("models/".$pagina.".php")){
+		echo "Falta definir la clase ".$pagina;
+		exit;
+	}  
+
 	if(is_file("views/".$pagina.".php")){
-		require_once("views/".$pagina.".php"); 
+		require_once("models/".$pagina.".php");
+		$purchase = new Purchases();
+
+		require_once("views/".$pagina.".php");
 	} else{
 		header('Location: 404.php');
 	}
--- controllers/ventas.php
+++ controllers/ventas.php
@@ -1,6 +1,18 @@
 <?php
+	if (!is_file("models/".$pagina.".php")){
+		echo "Falta definir la clase ".$pagina;
+		exit;
+	}  
+
 	if(is_file("views/".$pagina.".php")){
-		require_once("views/".$pagina.".php"); 
+		require_once("models/".$pagina.".php");
+		$purchase = new Purchases();
+
+		if (!empty($_POST)) {
+			$purchase->add($_POST['clients'], $_POST['idp'], $_POST['cant']);
+		}
+
+		require_once("views/".$pagina.".php");
 	} else{
 		header('Location: 404.php');
 	}
